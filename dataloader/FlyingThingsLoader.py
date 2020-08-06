8c430a2cd74fbb8ab13b8203edc8fb6cef8c486a
Djordje Batic <djordjebatic@gmail.com>
2020-08-06 13:43:51 +0000

    corrected decoding for python3

--- dataloader/FlyingThingsLoader.py
+++ dataloader/FlyingThingsLoader.py
@@ -3,4 +3,6 @@ from python_pfm import readPFM
 def load_disparity(file_path):
     return readPFM(file_path)
 
+if __name__ == '__main__':
+    print(load_disparity("sample_dataset/disparity/0006.pfm"))
 
